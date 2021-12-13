c90d7256d5a06776e247fe49f01f6717fe007e33
hamza <hamzabinsultan313@gmail.com>
2021-12-13 11:19:44 +0500

    ubdated

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1>my name is hamza aliand my father name is sultan nasir</h1>
+      <h1>my name is hamza aliand my father name is sultan 
+        and my friend is muneeb zaidi
+      </h1>
     </div>
   )
 }
