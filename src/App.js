97f226b67e7639d7bac5b1bb71e58a885cd5fe5c
hamza <hamzabinsultan313@gmail.com>
2021-12-14 19:31:32 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Appbar from './Appbar'
 
 const App = () => {
   return (
     <div>
+      <Appbar/>
       <h1>my name is hamza aliand my father name is sultan 
         and my friend is muneeb zaidi
       </h1>
